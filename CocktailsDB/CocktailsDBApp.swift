1251c0f6c60c1ca1b7d231ab62c713b57d0e8337
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-09 17:14:52 +0100

    Initial Commit

---
+++ CocktailsDB/CocktailsDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsDBApp.swift
+//  CocktailsDB
+//
+//  Created by Giorgio Giannotta on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
