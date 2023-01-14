af77f4e5ae38df032057d748894936072d258358
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-14 10:28:18 +0100

    Add cocktails details to DetailView

--- CocktailsDB/DetailView.swift
+++ CocktailsDB/DetailView.swift
@@ -14,6 +14,8 @@ struct DetailView: View {
     var body: some View {
         VStack {
             if drink != nil {
+                Text(drink!.strDrink)
+                    .font(.largeTitle)
                 AsyncImage(url: URL(string: drink!.strDrinkThumb)) { image in
                     image
                         .resizable()
@@ -21,12 +23,128 @@ struct DetailView: View {
                 } placeholder: {
                     Color.blue.opacity(0.1)
                 }
-                .frame(width: 250, height: 250)
+                .frame(width: 300, height: 300)
                 .cornerRadius(20)
-                Text(drink!.strDrink)
+                HStack {
+                    Text("Glass")
+                        .font(.title2)
+                    Text(drink!.strGlass)
+                        .font(.subheadline)
+                }
+                HStack {
+                    Text("Category")
+                        .font(.title2)
+                    Text(drink!.strCategory)
+                        .font(.subheadline)
+                }
+                
+                HStack {
+                    Text("Alcoholic")
+                        .font(.title2)
+                    Text(drink!.strAlcoholic)
+                        .font(.subheadline)
+                }
+                
+                Text("Instructions")
+                        .font(.title2)
                 Text(drink!.strInstructions)
-                Text(drink!.strIngredient1)
-                //aggiungere altri ingredienti e proprietà del drink
+                    .lineLimit(nil)
+                    .font(.subheadline)
+                
+                Text("Ingredients")
+                    .font(.title2)
+                
+                Group {
+                    HStack {
+                        Text(drink!.strIngredient1 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure1 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient2 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure2 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient3 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure3 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient5 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure5 ?? "")
+                            .font(.subheadline)
+                    }
+                
+                    HStack {
+                        Text(drink!.strIngredient6 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure6 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient7 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure7 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient8 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure8 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient9 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure9 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient10 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure10 ?? "")
+                            .font(.subheadline)
+                    }
+                }
+                Group {
+                    HStack {
+                        Text(drink!.strIngredient11 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure11 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient12 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure12 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient13 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure13 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient14 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure14 ?? "")
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        Text(drink!.strIngredient15 ?? "")
+                            .font(.subheadline)
+                        Text(drink!.strMeasure15 ?? "")
+                            .font(.subheadline)
+                    }
+                }
+                    
+                
             } else {
                 Text("Loading...")
             }
@@ -49,6 +167,6 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(idDrink: "12560")
+        DetailView(idDrink: "14588")
     }
 }
